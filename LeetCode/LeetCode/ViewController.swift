f01f7c5d76b42841e24a20610866bd8b4d5b0af2
liyuchen <1260197127@qq.com>
2020-08-30 18:45:12 +0800

    初始化代码

---
+++ LeetCode/LeetCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LeetCode
+//
+//  Created by 李玉臣 on 2020/8/30.
+//  Copyright © 2020 LYfinacial.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
